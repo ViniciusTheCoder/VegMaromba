14aed42cd787e2e4a3b747e2d8318448d75c2aa8
ViniciusTheCoder <vinigurskiferraz@gmail.com>
2023-02-27 00:38:17 -0300

    fix and  chore: refactoring header

--- src/vanilla.js
+++ src/vanilla.js
@@ -1,35 +1,10 @@
-const slides = document.querySelector(".slides");
-const slideImages = document.querySelectorAll(".slides img");
-const slideControls = document.querySelectorAll(".slide-control button");
+const searchIcon = document.getElementById("search-icon");
+const searchInput = document.getElementById("search-input");
 
-let slideIndex = 0;
-let interval = setInterval(changeSlide, 5000);
-
-function changeSlide() {
-  slideIndex++;
-  if (slideIndex >= slideImages.length) {
-    slideIndex = 0;
+searchIcon.addEventListener("click", function() {
+  if (searchInput.style.display === "none") {
+    searchInput.style.display = "block";
+  } else {
+    searchInput.style.display = "none";
   }
-  slides.style.transform = `translateX(-${slideIndex * 100}%)`;
-  updateSlideControls();
-}
-
-function updateSlideControls() {
-  slideControls.forEach((control, index) => {
-    if (index === slideIndex) {
-      control.classList.add("active");
-    } else {
-      control.classList.remove("active");
-    }
-  });
-}
-
-slideControls.forEach((control, index) => {
-  control.addEventListener("click", () => {
-    slideIndex = index;
-    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
-    clearInterval(interval);
-    interval = setInterval(changeSlide, 5000);
-    updateSlideControls();
-  });
 });
