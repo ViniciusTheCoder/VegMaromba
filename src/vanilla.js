0d0e360d4802f923f98e7534f9f184793fc70525
ViniciusTheCoder <vinigurskiferraz@gmail.com>
2023-02-27 01:51:34 -0300

    fix: fixing image slider layout

--- src/vanilla.js
+++ src/vanilla.js
@@ -8,3 +8,20 @@ searchIcon.addEventListener("click", function() {
     searchInput.style.display = "none";
   }
 });
+
+let count = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval( function(){
+  nextImage();
+}, 5000)
+
+function nextImage(){
+  count++;
+  if (count > 4) {
+    count = 1;
+  }
+
+  document.getElementById('radio'+count).checked = true;
+
+}
