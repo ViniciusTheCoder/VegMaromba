a77c8575db4b2dc23a87bd3bb93c019a3ad20ed5
ViniciusTheCoder <vinigurskiferraz@gmail.com>
2023-02-28 22:15:25 -0300

    feat: re-stylin' LP

--- src/vanilla.js
+++ src/vanilla.js
@@ -1,14 +1,3 @@
-const searchIcon = document.getElementById("search-icon");
-const searchInput = document.getElementById("search-input");
-
-searchIcon.addEventListener("click", function() {
-  if (searchInput.style.display === "none") {
-    searchInput.style.display = "block";
-  } else {
-    searchInput.style.display = "none";
-  }
-});
-
 let count = 1;
 document.getElementById('radio1').checked = true;
 
